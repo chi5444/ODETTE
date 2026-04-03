ea0313790cc25add64d9d2d57e6e0866f4aaf94f
chi5444 <101504281+chi5444@users.noreply.github.com>
2026-04-03 16:06:41 +0100

    Delete js directory

--- js/supabase.js
+++
@@ -1,4 +0,0 @@
-// js/supabase.js — ODETTE Store
-const SUPABASE_URL = 'https://aujfvtilrnfixbitrllc.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF1amZ2dGlscm5maXhiaXRybGxjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUyMDQyNDMsImV4cCI6MjA5MDc4MDI0M30.6nwkT7C-LRUqoEM3dctznVKOYvRcRW2NEyc1WPgtWms';
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
