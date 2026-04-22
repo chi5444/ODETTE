d996d576d7a548e1815e9a6878641040e9a8175a
chi5444 <101504281+chi5444@users.noreply.github.com>
2026-04-22 14:50:37 +0100

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-const SUPABASE_URL  = 'https://yhfxowgclcziyniodogw.supabase.co/rest/v1/';
+const SUPABASE_URL  = 'https://yhfxowgclcziyniodogw.supabase.co';
 const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InloZnhvd2djbGN6aXluaW9kb2d3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4NTY5NzEsImV4cCI6MjA5MjQzMjk3MX0.dt7iJZg44lXyyjsP62wena9_MNWXLBTI1KtEZXtYUxA';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON);
