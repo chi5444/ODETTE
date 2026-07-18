7d3f993695cb79e69e37bc697a4d9d9618b16d66
chi5444 <101504281+chi5444@users.noreply.github.com>
2026-07-18 23:51:21 +0100

    Update Supabase URL and anon key

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-const SUPABASE_URL  = 'https://yhfxowgclcziyniodogw.supabase.co';
-const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InloZnhvd2djbGN6aXluaW9kb2d3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4NTY5NzEsImV4cCI6MjA5MjQzMjk3MX0.dt7iJZg44lXyyjsP62wena9_MNWXLBTI1KtEZXtYUxA';
+const SUPABASE_URL  = 'https://phnaknysszqokeeemsro.supabase.co/rest/v1/';
+const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBobmFrbnlzc3pxb2tlZWVtc3JvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ0MDc3ODQsImV4cCI6MjA5OTk4Mzc4NH0.Jqg4FMzg4LCpJ4fqiPIbrhyfHgxSXuz2415JCft7i60';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON);
 export const ADMIN_EMAIL = 'admin@odette.com';
